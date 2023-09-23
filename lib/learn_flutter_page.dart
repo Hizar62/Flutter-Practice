8636d9c98d13ffb02c934d63906f273c67c8f98a
Hizar62 <hizarsajjad632@gmail.com>
2023-09-23 18:05:27 +0500

    Adding Content

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -8,6 +8,8 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool? isCheckBox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,16 +20,120 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
             onPressed: () {
               Navigator.of(context).pop();
             },
-            icon: Icon(Icons.arrow_back_ios_new)),
+            icon: const Icon(Icons.arrow_back_ios_new)),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.info_outline))],
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Image(
-              image: AssetImage('images/Human.jpg'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/Human.jpg'),
+            const SizedBox(
+              height: 10,
             ),
-          )
-        ],
+            const Divider(),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.grey,
+              child: const Center(
+                child: Text('This is Text Widget',
+                    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
+              ),
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: isSwitch ? Colors.green : Colors.amber),
+                onPressed: () {
+                  debugPrint("Eleveted Button");
+                },
+                child: const Text("Elevated Button")),
+            OutlinedButton(
+                onPressed: () {
+                  debugPrint("Outlined-Button Button");
+                },
+                child: const Text("Outlined-Button Button")),
+            TextButton(
+              onPressed: () {
+                debugPrint("Text-Button Button");
+              },
+              child: const Text("Text-Button Button"),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  Icons.local_fire_department,
+                  color: Color.fromARGB(255, 8, 123, 239),
+                ),
+                Text('Row Widget'),
+                Icon(
+                  Icons.local_fire_department,
+                  color: Color.fromARGB(255, 8, 123, 239),
+                )
+              ],
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newbool) {
+                  setState(() {
+                    isSwitch = newbool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckBox,
+                onChanged: (bool? newcheck) {
+                  setState(() {
+                    isCheckBox = newcheck;
+                  });
+                }),
+            Image.network(
+                'https://wallpapers.com/images/featured/creative-0ho3780ng1sfoloq.jpg'),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.grey,
+              child: const Center(
+                child: Text('This is Text Widget',
+                    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
+              ),
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: isSwitch ? Colors.green : Colors.amber),
+                onPressed: () {
+                  debugPrint("Eleveted Button");
+                },
+                child: const Text("Elevated Button")),
+            OutlinedButton(
+                onPressed: () {
+                  debugPrint("Outlined-Button Button");
+                },
+                child: const Text("Outlined-Button Button")),
+            TextButton(
+              onPressed: () {
+                debugPrint("Text-Button Button");
+              },
+              child: const Text("Text-Button Button"),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  Icons.local_fire_department,
+                  color: Color.fromARGB(255, 8, 123, 239),
+                ),
+                Text('Row Widget'),
+                Icon(
+                  Icons.local_fire_department,
+                  color: Color.fromARGB(255, 8, 123, 239),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            )
+          ],
+        ),
       ),
     );
   }
